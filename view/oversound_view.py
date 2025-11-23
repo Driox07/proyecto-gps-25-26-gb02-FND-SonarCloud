7cf6465283163a63c4383964674b4396a5b8a71a
Driox07 <82775672+Driox07@users.noreply.github.com>
2025-11-23 20:48:17 +0100

    Squashed commit of the following:
    commit 8632e07669a6947dddcd99ab105bb455d6ed7000
    Author: Driox07 <82775672+Driox07@users.noreply.github.com>
    Date:   Sun Nov 23 20:42:53 2025 +0100
    
        SCRUM-83 ST-36 Implementar sistema de compra de canción

--- view/oversound_view.py
+++ view/oversound_view.py
@@ -25,3 +25,34 @@ class View():
         data = {"userdata": userdata, "syu_server": syu_server}
         return templates.TemplateResponse("label.html", {"request": request, "data": data, "label": label_info, "is_owner": is_owner, "is_member": is_member})
     
+
+    # Renderizar la template shop.html
+    def get_shop_view(self, request: Request, userdata: dict, songs, genres, artistas, albums, merch, artists_map=None, genres_map=None, tya_server=None):
+        if artists_map is None:
+            artists_map = {}
+        if genres_map is None:
+            genres_map = {}
+        data = {"userdata": userdata}
+        return templates.TemplateResponse("shop.html", {
+            "request": request, 
+            "data": data,
+            "songs": songs,
+            "albums": albums,
+            "merch": merch,
+            "genres": genres,
+            "artists": artistas,
+            "artists_map": artists_map, 
+            "genres_map": genres_map,
+            "tya_server": tya_server
+        })
+    
+    def get_songs_view(self, request: Request, songs):
+        return templates.TemplateResponse("main/index.html", {"request" :request, "songs" : songs})
+    
+    def get_song_view(self, request: Request, song_info : dict, tipoUsuario: int, user : dict, isLiked: bool, inCarrito: bool, syu_server: str = None, metrics: dict = None, tya_server: str = None, rye_server: str = None, pt_server: str = None):
+        data = {"userdata": user, "syu_server": syu_server, "pt_server": pt_server, "song": song_info}
+        return templates.TemplateResponse("song.html", {"request": request, "data": data, "tipoUsuario": tipoUsuario, "user": user, "isLiked": isLiked, "inCarrito": inCarrito, "stats": metrics, "syu_server": syu_server, "tya_server": tya_server, "rye_server": rye_server})
+
+    # Renderizar la template purchased.html
+    def get_purchased_view(self, request: Request, user, songs):
+        return templates.TemplateResponse("shop/purchased.html", {"request": request, "usuario": user, "songs": songs})
